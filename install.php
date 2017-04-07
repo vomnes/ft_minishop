58fbc1ac4a00b35aac5bd474169ed6f62c7635ae
Valentin OMNES <vomnes@student.42.fr>
2017-04-07 23:48:55 +0200

    Add install.php

--- install.php
+++ install.php
@@ -1,3 +1,12 @@
 <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
 
+    $connection = mysqli_connect($servername, $username, $password);
+    if (!$conn)
+    {
+      die ("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
 ?>
