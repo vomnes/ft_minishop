945f8931d82c8b6c3a161618c0132fbd4ade92b4
Valentin OMNES <vomnes@student.42.fr>
2017-04-09 04:13:08 +0200

    Add admin

---
+++ front_end/admin.php
@@ -0,0 +1,36 @@
+<html lang="fr">
+  <meta charset="UTF-8">
+<head>
+  <title>ft_minishop</title>
+  <link rel="stylesheet" type="text/css" href="admin.css">
+  <link href="https://fonts.googleapis.com/css?family=Lobster|Press+Start+2P" rel="stylesheet">
+</head>
+<body>
+  <div class="header">
+  <img src="http://art42.fr/resources/42_logo_white.png" style="margin-left:20px; margin-top:7px"; width=70px;>
+  <T3><a href="./index.php">ft_minishop</a></T3>
+  <div id="nav-menu">
+  <T1>
+    <nav>
+      <ul>
+        <li><a href="../back_end/backend_logout.php">Admin Session - Logout</a></li>
+      </ul>
+    </T1>
+    </nav>
+  </div>
+  </div>
+  <center class="container">
+    <div>
+      <div class="block" id="middle_block">
+
+      </div>
+    </div>
+  </center>
+  <br/ >
+
+  <hr id="line"/>
+  <p id="sign"/>
+   © vomnes/jpeguet 2017
+  </p>
+  </body>
+</html>
--- front_end/cart.php
+++ front_end/cart.php
@@ -1,5 +1,4 @@
 <?php include('standard_upper_html.php'); ?>
-</div>
 <div class="block" id="middle_block">
 
 	<T2>Your actual cart :</T2></br >
